a1b62d26ca9ec5cc62273af51069b3acfc0ad815
Daniel Braun <dbraun86@gmail.com>
2013-03-18 14:39:31 +0200

    updated heroku settings

--- sidar/settings.py
+++ sidar/settings.py
@@ -1,9 +1,10 @@
 import socket
 import os
 
-if 'heroku' in os.environ['PATH']:
-    from sidar.settings_heroku import *
-    print "Using heroku server settings."
+if os.environ.get('LD_LIBRARY_PATH'):
+    if 'heroku' in os.environ.get('LD_LIBRARY_PATH'):
+        from sidar.settings_heroku import *
+        print "Using heroku server settings."
 elif socket.gethostname() == "design25":
     from sidar.settings_staging import *
     print "Using staging server settings."
