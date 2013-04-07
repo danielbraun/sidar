cecf7f3e99094a6b199f251f7fba3f34b55c30d3
Daniel Braun <dbraun86@gmail.com>
2013-04-07 11:30:18 +0300

    Fixed field size for Books, so importdata command can work

--- bibliography/models.py
+++ bibliography/models.py
@@ -17,12 +17,12 @@ class BookCategory(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(u'כותרת', max_length=128)
+    title = models.CharField(u'כותרת', max_length=256)
     discipline = models.ForeignKey(Discipline, verbose_name=u'מחלקת עיצוב')
     category = models.ForeignKey(BookCategory, verbose_name=u'קטגוריה')
-    comments = models.CharField(u'הערות', max_length=128)
-    author_as_text = models.CharField(u'מחבר/עורך/מאייר', max_length=128)
-    publication_as_text = models.CharField(u'הוצאה/מקום/שנה', max_length=128)
+    comments = models.CharField(u'הערות', max_length=256)
+    author_as_text = models.CharField(u'מחבר/עורך/מאייר', max_length=256)
+    publication_as_text = models.CharField(u'הוצאה/מקום/שנה', max_length=256)
 
     class Meta:
         verbose_name = u'ספר'
