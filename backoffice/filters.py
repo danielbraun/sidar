73064ee338d94fd6e2419f9aa74981d844ef4bbb
Daniel Braun <dbraun86@gmail.com>
2013-06-12 11:10:55 +0300

    שיפור חיפוש מתקדם
    התאמות קטנות לפי מה שסוכם.
    fixes #75

--- backoffice/filters.py
+++ backoffice/filters.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
 import django_filters
-from .models import Work
+from .models import Work, Subject, Collector
 
 
 class WorkFilterSet(django_filters.FilterSet):
+    subjects = django_filters.ModelChoiceFilter(
+        queryset=Subject.objects.all(),
+        label=u'נושא'
+    )
+
+    of_collections = django_filters.ModelChoiceFilter(
+        queryset=Collector.objects.all(),
+        label=u'מאוסף'
+    )
+
     class Meta:
         model = Work
-        fields = ['designer', 'category', 'subjects', 'name_he',
-                  'of_collections', 'publish_year', 'description_he']
+        fields = ['designer', 'of_collections', 'category', 'subjects',
+                  'name_he', 'publish_year']
