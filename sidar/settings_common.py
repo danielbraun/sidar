5fed3cc77054427200069891e8336e9f882ee37c
Daniel Braun <dbraun86@gmail.com>
2013-08-07 15:03:44 +0300

    Added debug toolbar, updated sidar apps

--- sidar/settings_common.py
+++ sidar/settings_common.py
@@ -15,7 +15,7 @@ SOUTH_TESTS_MIGRATE = False
 TEMPLATE_DEBUG = DEBUG
 
 LOGIN_URL = '/'
-
+INTERNAL_IPS = ('127.0.0.1',)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -130,6 +130,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.locale.LocaleMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'sidar.urls'
@@ -181,6 +182,7 @@ INSTALLED_APPS = (
     'videos',
     'events',
     'moderation',
+    'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
